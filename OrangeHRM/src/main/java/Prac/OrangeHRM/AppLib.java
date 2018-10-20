a68c7aa0cbcc13c09b5d39ca664f9660fbcb9ca5
soumik-rickysaha <soumik.rickysaha@gmail.com>
2018-10-20 22:35:39 +0530

    Changed the browser start methods. Added two methods depending on the type on Browser launch required

--- OrangeHRM/src/main/java/Prac/OrangeHRM/AppLib.java
+++ OrangeHRM/src/main/java/Prac/OrangeHRM/AppLib.java
@@ -21,28 +21,36 @@ class AppLib {
 	public DesiredCapabilities cap = DesiredCapabilities.chrome();
 	public String ProjectPath = System.getProperty("user.dir");
 
-	public AppLib() throws MalformedURLException{
-for (int i = 0; i < 6; i++) {
-	//		System.setProperty("webdriver.chrome.driver", ProjectPath.replace("\\", "\\\\") + "\\Drivers\\chromedriver.exe");
-	//		driver=new ChromeDriver();
-	//		wait = new WebDriverWait(driver, 30);
-	//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	//		driver.manage().window().maximize();
-	try {
-		String nodeURL = "http://192.168.0.9:4445/wd/hub";
-		//			ChromeOptions options = new ChromeOptions();
-		FirefoxOptions options = new FirefoxOptions();
-		//		driverOptions.setBrowserName("chrome");
-		//        capability.setPlatform(Platform.WIN10);
-		driver = new RemoteWebDriver(new URL(nodeURL), options);
-	} catch (Exception e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	} 
-}
+	public AppLib() throws MalformedURLException {
+		
+		NormalRun();
+	}
 
+	public void NormalRun() {
+		System.setProperty("webdriver.chrome.driver",ProjectPath.replace("\\", "\\\\") + "\\Drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		wait = new WebDriverWait(driver, 30);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	public void RunViaGrid() {
+		for (int i = 0; i < 6; i++) {
+
+			try {
+				String nodeURL = "http://192.168.0.9:4445/wd/hub";
+				// ChromeOptions options = new ChromeOptions();
+				FirefoxOptions options = new FirefoxOptions();
+				// driverOptions.setBrowserName("chrome");
+				// capability.setPlatform(Platform.WIN10);
+				driver = new RemoteWebDriver(new URL(nodeURL), options);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-	
+	}
+
 	public WebDriver getDriver() {
 		return driver;
 	}
